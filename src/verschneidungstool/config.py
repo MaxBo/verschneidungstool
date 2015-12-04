8db72059a3e9921c01b8810a9df752999100fd2b
MaxBo <max.bohnet@tuhh.de>
2015-12-04 22:29:11 +0100

    default psql_path to dlls subdirectory

--- src/verschneidungstool/config.py
+++ src/verschneidungstool/config.py
@@ -11,12 +11,12 @@ setting_struct = {
         'db_name': 'db',
         'host': 'localhost',
         'port': '5432',
-        'srid': '3044'
+        'srid': DEFAULT_SRID,
         },
     'env': {
-        'psql_path': '',
-        'shp2pgsql_path': '',
-        'pgsql2shp_path': ''
+        'psql_path': 'dlls/psql.exe',
+        'shp2pgsql_path': 'dlls/shp2pgsql.exe',
+        'pgsql2shp_path': 'dlls/pgsql2shp.exe'
         }
 }
 
@@ -37,10 +37,11 @@ class Config(Borg):
     def __init__(self):
         Borg.__init__(self)
 
-    '''
-    read the config from given xml file (default config.xml)
-    '''
     def read(self, filename=None):
+        '''
+        read the config from given xml file (default config.xml)
+        '''
+
         self.filename = filename if filename else DEFAULT_FILE
 
         # create file if it does not exist
@@ -48,25 +49,25 @@ class Config(Borg):
             self.settings = copy.deepcopy(setting_struct)
             self.write(self.filename)
         tree = etree.parse(self.filename)
-        self.settings = copy.deepcopy(setting_struct) 
+        self.settings = copy.deepcopy(setting_struct)
         f_set = xml_to_dict(tree.getroot())
-        for key, value in f_set.iteritems():            
+        for key, value in f_set.iteritems():
             self.settings[key].update(value)
-
-    '''
-    write the config as xml to given file (default config.xml)
-    '''
     def write(self, filename=None):
+        '''
+        write the config as xml to given file (default config.xml)
+        '''
+
         xml_tree = etree.Element('CONFIG')
         dict_to_xml(xml_tree, self.settings)
         if not filename:
             filename = self.filename
         etree.ElementTree(xml_tree).write(str(filename), pretty_print=True)
 
-'''
-append the entries of a dictionary as childs to the given xml tree element
-'''
 def dict_to_xml(element, dictionary):
+    '''
+    append the entries of a dictionary as childs to the given xml tree element
+    '''
     if isinstance(dictionary, list):
         for value in dictionary:
             elem = etree.Element('value')
@@ -81,11 +82,11 @@ def dict_to_xml(element, dictionary):
             element.append(elem)
             dict_to_xml(elem, dictionary[key])
 
-'''
-convert a xml tree to a dictionary
-represented_as_arrays: list of Strings, all XML Tags, which should be handled as arrays
-'''
 def xml_to_dict(tree, represented_as_arrays=[]):
+    '''
+    convert a xml tree to a dictionary
+    represented_as_arrays: list of Strings, all XML Tags, which should be handled as arrays
+    '''
     if tree.tag in represented_as_arrays:
         value = []
         for child in tree.getchildren():
