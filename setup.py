04390f814a8d4de0dd9adc62c9e82266e4bf8b94
MaxBo <bohnet@ggr-planung.de>
2016-04-19 17:55:21 +0200

    version 1.0.1 tagged

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="verschneidungstool",
-    version="1.0",
+    version="1.0.1",
     url='https://github.com/MaxBo/Verkehrsmodell',
     author='Christoph Franke',
     description="graphical user interface for computing intersections in the region of Hannover",
