552a76e44baf467b28553a46a6f16aa0a1815d39
Max Bohnet <bohnet@ggr-planung.de>
2020-04-03 22:35:09 +0200

    use kind instead of char

--- src/verschneidungstool/save2visumtransfer.py
+++ src/verschneidungstool/save2visumtransfer.py
@@ -7,13 +7,13 @@ def save_to_visum_transfer(df: pd.DataFrame,
                            filepath: str):
     userdefined = BenutzerdefiniertesAttribut(mode='+')
     zones = Bezirke(mode='*')
-    dtype2datatype = {'d': 'Double',
-                      'q': 'Integer',
+    dtype2datatype = {'f': 'Double',
+                      'i': 'Integer',
                       'O': 'Text', }
 
     for colname in df.columns:
         col = df[colname]
-        datatype = dtype2datatype.get(col.dtype.char, 'Double')
+        datatype = dtype2datatype.get(col.dtype.kind, 'Double')
         userdefined.add_daten_attribute('BEZIRK', colname, datentyp=datatype)
 
     df.index.name = zones.pkey
