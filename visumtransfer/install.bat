d978c729482bf045e20a51cad635d3d1920f2d23
MaxBo <bohnet@ggr-planung.de>
2021-09-15 19:02:01 +0200

    install.bat for  visumtransfer

--- visumtransfer/install.bat
+++ visumtransfer/install.bat
@@ -1,16 +1,2 @@
-call conda create -y -n generatevisem python=3.5
-call activate generatevisem
-call conda install -y pandas
-call conda install -y pytables
-call conda install -y h5py
-call conda install -y netcdf4
-call pip install h5netcdf
-call conda install -y -c jiffyclub zbox xarray
-call pip install orca
-call conda install -y psycopg2
-call conda install -y sqlalchemy
-call pip install recordclass
-call conda install -y openpyxl
-call conda install -y -c conda-forge pyqt
-call pip install ..\wheels\ViTables-2.1-py3-none-any.whl
-call pip install -e .
+call conda activate verschneidungstool39
+call pip install .
