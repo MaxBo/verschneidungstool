c6529abcf6336c48511befb49423c59d6649a717
MaxBo <bohnet@ggr-planung.de>
2022-04-06 12:08:24 +0200

    delete sql

--- install_gui.bat
+++ install_gui.bat
@@ -1,9 +1,9 @@
 call conda create -y -n verschneidungstool39 python=3.9 setuptools pyqt=5 numpy psycopg2 xlwt lxml pip pandas openpyxl xarray pytest pytables
 call conda activate verschneidungstool39
 cd visumtransfer
-call pip install .
+call python -m pip install .
 cd ..\verschneidungstool
-call pip install .
+call python -m pip install .
 cd ..
 echo.
 echo.
