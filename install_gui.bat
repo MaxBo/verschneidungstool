f7fc9775222172b904944b933f3241ee9e636a41
MaxBo <bohnet@ggr-planung.de>
2023-01-08 23:35:04 +0100

    installer angepasst

--- install_gui.bat
+++ install_gui.bat
@@ -1,11 +1,10 @@
-REM call conda create -y -n verschneidungstool39
-REM python=3.9 setuptools pyqt=5 numpy psycopg2 xlwt lxml pip pandas openpyxl xarray pytest pytables sqlalchemy
-call conda activate strukturdaten
+call conda create -y -n verschneidungstool_2023 python=3.9 setuptools pyqt=5 numpy psycopg2 xlwt lxml pip pandas openpyxl xarray pytest pytables sqlalchemy
+call conda activate verschneidungstool_2023
 call python -m pip install -r requirements.txt
-cd visumtransfer
+cd visumtransfer\visumtransfer
 call pip install .
-cd ..\verschneidungstool
-call pip install -e .
+cd ..\..\verschneidungstool
+call pip install .
 cd ..
 echo.
 echo.
