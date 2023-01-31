ecfef12a3d944850377313287df703379b6e0dee
MaxBo <bohnet@ggr-planung.de>
2023-01-31 23:56:41 +0100

    installer reghan

--- install_gui.bat
+++ install_gui.bat
@@ -1,5 +1,5 @@
-call conda create -y -n verschneidungstool39 python=3.9 setuptools pyqt=5 numpy psycopg2 xlwt lxml pip pandas openpyxl xarray pytest pytables
-call conda activate verschneidungstool39
+call conda create -y -n verschneidungstool_reghan python=3.9 setuptools pyqt=5 numpy psycopg2 xlwt lxml pip pandas openpyxl xarray pytest pytables
+call conda activate verschneidungstool_reghan
 cd visumtransfer
 call python -m pip install .
 cd ..\verschneidungstool
