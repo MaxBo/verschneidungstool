600b88b746cc664f9297e261016b3fe6d0655d83
VanessaDunk <dunker@ggr-planung.de>
2024-11-07 16:02:20 +0100

    pyqt 5

--- install_gui.bat
+++ install_gui.bat
@@ -1,4 +1,4 @@
-call mamba create -y -n verschneidungstool_310_2023 python=3.10 setuptools pyqt=5.12 numpy psycopg2 xlwt lxml pip pandas openpyxl xarray pytest pytables sqlalchemy
+call mamba create -y -n verschneidungstool_310_2023 python=3.10 setuptools pyqt=5 numpy psycopg2 xlwt lxml pip pandas openpyxl xarray pytest pytables sqlalchemy
 call mamba activate verschneidungstool_310_2023
 call python -m pip install -r requirements.txt
 cd visumtransfer\visumtransfer
