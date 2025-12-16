791f5123de167a2c1146a6444ef3664882f62181
Sophie <luettger@ggr-planung.de>
2025-12-16 16:18:22 +0100

    use python 3.13 (recordclass not ready for 3.14) and install pytables and lxml via pip

--- install_gui.bat
+++ install_gui.bat
@@ -1,5 +1,5 @@
-call mamba create -y -n verschneidungstool_314 python=3.14 setuptools pyqt=5 numpy psycopg2 xlwt lxml pip pandas openpyxl xarray pytest pytables sqlalchemy
-call mamba activate verschneidungstool_314
+call mamba create -y -n verschneidungstool_313 python=3.13 setuptools pyqt=5 numpy psycopg2 xlwt pip pandas openpyxl xarray pytest sqlalchemy
+call mamba activate verschneidungstool_313
 call python -m pip install -r requirements.txt
 cd visumtransfer\visumtransfer
 call pip install .
