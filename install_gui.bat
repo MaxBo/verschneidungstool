062c210982f83f5a4e60335279c7f01605ab2b2e
Max Bohnet <bohnet@ggr-planung.de>
2026-02-06 14:53:56 +0100

    downgrade to py3.12/np1.26

--- install_gui.bat
+++ install_gui.bat
@@ -1,5 +1,5 @@
-call mamba create -y -n verschneidungstool_313 python=3.13 setuptools pyqt=5 numpy psycopg2 xlwt pip pandas openpyxl xarray pytest sqlalchemy
-call mamba activate verschneidungstool_313
+call mamba create -y -n verschneidungstool_312_np1_26 python=3.12 setuptools pyqt=5 numpy=1.26 pandas=2.3 psycopg2 xlwt pip pandas openpyxl xarray pytest sqlalchemy
+call mamba activate verschneidungstool_312_np1_26
 call python -m pip install -r requirements.txt
 cd visumtransfer\visumtransfer
 call pip install .
